bb1fdca167177f0f650a48f605fc062ca839cc2a
Richard Tingle <98174673+oneMillionWorlds@users.noreply.github.com>
2022-07-29 18:30:35 +0100

    Improve player movement utilities

--- src/main/java/com/onemillionworlds/tamarin/TamarinUtilities.java
+++ src/main/java/com/onemillionworlds/tamarin/TamarinUtilities.java
@@ -9,16 +9,36 @@ import com.jme3.scene.Node;
 public class TamarinUtilities{
 
     /**
+     * Often you'll want to move the player to a particular location, however, the players head should never be moved
+     * directly (as it is defined by the headset) instead the section of the real world that overlaps with the virtual
+     * world should be moved such that the headset ends up requesting what you wanted anyway. This method achieves that
+     * @param vrAppState the VRAppState
+     * @param feetPosition where the players feet should be after the move
+     */
+    public static void movePlayerFeetToPosition(VRAppState vrAppState, Vector3f feetPosition){
+        Vector3f playerCurrentPosition = vrAppState.getVRViewManager().getLeftCamera().getLocation().add(vrAppState.getVRViewManager().getRightCamera().getLocation()).mult(0.5f);
+        Node observerNode = (Node)vrAppState.getObserver();
+        Vector3f movement = feetPosition.subtract(playerCurrentPosition);
+
+        Vector3f currentObserverPosition = observerNode.getLocalTranslation();
+
+        currentObserverPosition.x+=movement.x;
+        currentObserverPosition.z+=movement.z;
+        currentObserverPosition.y = feetPosition.y;
+    }
+
+    /**
      * Often you'll want to programatically turn the player, which should be done by rotating the observer.
      *
      * However, if the player isn't standing directly above the observer this rotation will induce motion.
      *
      * This method corrects for that and gives the impression the player is just turning
-     * @param observerNode the node that represents the observer (see tamarin wiki for explanation on observer node)
      * @param vrAppState the VRAppState
      * @param angleAboutYAxis the requested turn angle. Positive numbers turn left, negative numbers turn right
      */
-    public static void rotateObserverWithoutMovingPlayer(Node observerNode, VRAppState vrAppState, float angleAboutYAxis){
+    public static void rotateObserverWithoutMovingPlayer(VRAppState vrAppState, float angleAboutYAxis){
+        Node observerNode = (Node)vrAppState.getObserver();
+
         Quaternion currentRotation = observerNode.getLocalRotation();
         Quaternion leftTurn = new Quaternion();
         leftTurn.fromAngleAxis(angleAboutYAxis, Vector3f.UNIT_Y);
@@ -33,6 +53,23 @@ public class TamarinUtilities{
 
         Vector3f inducedError = playerPositionAfterRotation.subtract(playerStartPosition);
         observerNode.setLocalTranslation(observerNode.getLocalTranslation().subtract(inducedError));
+    }
+
+    /**
+     * Often you'll want to programatically turn the player, which should be done by rotating the observer.
+     *
+     * However, if the player isn't standing directly above the observer this rotation will induce motion.
+     *
+     * Deprecated, no need to pass observerNode
+     *
+     * This method corrects for that and gives the impression the player is just turning
+     * @param observerNode the node that represents the observer (see tamarin wiki for explanation on observer node)
+     * @param vrAppState the VRAppState
+     * @param angleAboutYAxis the requested turn angle. Positive numbers turn left, negative numbers turn right
+     */
+    @Deprecated
+    public static void rotateObserverWithoutMovingPlayer(Node observerNode, VRAppState vrAppState, float angleAboutYAxis){
+        rotateObserverWithoutMovingPlayer(vrAppState, angleAboutYAxis);
     }
 
 }
