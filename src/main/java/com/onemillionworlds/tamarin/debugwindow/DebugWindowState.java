c55ec83bd59c79a792b8cfad58400c26dca83851
Richard Tingle <98174673+oneMillionWorlds@users.noreply.github.com>
2022-08-05 18:41:49 +0100

    Abstract out debug line items to move towards supporting buttons and other data

--- src/main/java/com/onemillionworlds/tamarin/debugwindow/DebugWindowState.java
+++ src/main/java/com/onemillionworlds/tamarin/debugwindow/DebugWindowState.java
@@ -5,6 +5,8 @@ import com.jme3.app.SimpleApplication;
 import com.jme3.app.StatsAppState;
 import com.jme3.app.VRAppState;
 import com.jme3.app.state.BaseAppState;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
@@ -12,10 +14,13 @@ import com.onemillionworlds.tamarin.vrhands.grabbing.AbstractGrabControl;
 import com.onemillionworlds.tamarin.vrhands.grabbing.RelativeMovingGrabControl;
 import com.simsilica.lemur.Container;
 import com.simsilica.lemur.Label;
+import lombok.AllArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -38,8 +43,18 @@ public class DebugWindowState extends BaseAppState{
     Node debugWindowNode = new Node("Tamarin-debug-window-node");
     Container lemurWindow = new Container();
 
-    Map<String, String> dataTable = new LinkedHashMap<>();
-    Map<String, Label> labels = new HashMap<>();
+    /**
+     * Stuff this window is displaying
+     */
+    Map<String, String> displayDataTable = new LinkedHashMap<>();
+    Map<String, Integer> ticksSinceLastSet = new HashMap<>();
+
+    /**
+     * Stuff this window is providing a UI to enter
+     */
+    Map<String, Object> userEntryDataTable = new LinkedHashMap<>();
+    Map<String, LineItem> newLineItems = new LinkedHashMap<>();
+    Map<String, LineItem> currentLineItems = new HashMap<>();
 
     VRAppState vrAppState;
     StatsAppState statsAppState;
@@ -53,9 +68,6 @@ public class DebugWindowState extends BaseAppState{
     private static final float MAX_DISTANCE_FROM_PLAYER = 2;
 
     double timeTillNextPositionCheck = 0;
-
-    @Setter
-    boolean showFps = true;
 
     public DebugWindowState(){
         assert INSTANCE == null : "Can only have 1 DebugWindowState";
@@ -93,13 +105,22 @@ public class DebugWindowState extends BaseAppState{
         debugWindowNode.setCullHint(Spatial.CullHint.Always);
     }
 
+    public void showFtps(){
+        newLineItems.put("FPS", new NonFadingRenderText("FPS"));
+    }
+
     public void setData(String dataLabel, Object data){
         setData(dataLabel, data.toString());
     }
 
     public void setData(String dataLabel, String data){
         synchronized(lock){
-            dataTable.put(dataLabel, data);
+            if (!currentLineItems.containsKey(dataLabel) && !newLineItems.containsKey(dataLabel)){
+                newLineItems.put(dataLabel, new FadingRenderText(dataLabel));
+            }
+
+            displayDataTable.put(dataLabel, data);
+            ticksSinceLastSet.put(dataLabel, 0);
         }
     }
 
@@ -107,27 +128,29 @@ public class DebugWindowState extends BaseAppState{
     public void update(float tpf){
         super.update(tpf);
 
-        if (showFps) {
-            secondCounter += getApplication().getTimer().getTimePerFrame();
-            frameCounter ++;
-            if (secondCounter >= 1.0f) {
-                int fps = (int) (frameCounter / secondCounter);
-                setData("FPS", ""+fps);
-                secondCounter = 0.0f;
-                frameCounter = 0;
-            }
+        secondCounter += getApplication().getTimer().getTimePerFrame();
+        frameCounter ++;
+        if (secondCounter >= 1.0f) {
+            int fps = (int) (frameCounter / secondCounter);
+            setData("FPS", ""+fps);
+            secondCounter = 0.0f;
+            frameCounter = 0;
         }
+
         synchronized(lock){
-            for(Map.Entry<String, String> dataEntry : dataTable.entrySet()){
-                Label label = labels.get(dataEntry.getKey());
-                if(label == null){
-                    label = new Label("");
-                    lemurWindow.addChild(label);
-                }
-                labels.put(dataEntry.getKey(), label);
-                label.setText(dataEntry.getKey() + ": " + dataEntry.getValue());
+            if (!newLineItems.isEmpty()){
+                newLineItems.forEach((label, lineItem) -> {
+                    lemurWindow.addChild(lineItem.render());
+                    currentLineItems.put(label, lineItem);
+                });
             }
+
+            currentLineItems.forEach((label, lineItem) -> {
+                lineItem.update(tpf);
+            });
         }
+
+        ticksSinceLastSet.replaceAll((key, ticks) -> ticks+1);
 
         timeTillNextPositionCheck-=tpf;
         if (timeTillNextPositionCheck<=0){
@@ -148,5 +171,58 @@ public class DebugWindowState extends BaseAppState{
 
     private static Vector3f getVrCameraPosition(VRAppState vrAppState){
         return vrAppState.getVRViewManager().getLeftCamera().getLocation().add(vrAppState.getVRViewManager().getRightCamera().getLocation()).mult(0.5f);
+    }
+
+    private class NonFadingRenderText extends LineItem{
+        Label labelNode = new Label("");
+
+        public NonFadingRenderText(String label){
+            super(label);
+        }
+
+        @Override
+        public Node render(){
+            return labelNode;
+        }
+
+        @Override
+        public void update(double timeslice){
+            super.update(timeslice);
+            labelNode.setText(label + ": " + displayDataTable.get(label));
+        }
+    }
+
+    private class FadingRenderText extends NonFadingRenderText{
+        public FadingRenderText(String label){
+            super(label);
+        }
+
+        @Override
+        public void update(double timeslice){
+            super.update(timeslice);
+            int noOfticksSinceLastSet = ticksSinceLastSet.get(label);
+
+            if (noOfticksSinceLastSet<3){
+                labelNode.setColor(ColorRGBA.White);
+            }else{
+                float brightness = 1-noOfticksSinceLastSet/(2*60f);
+                brightness = FastMath.clamp(brightness, 0.3f, 1);
+
+                ColorRGBA fadedColour = ColorRGBA.fromRGBA255((int)(255*brightness), (int)(255*brightness),(int)(255*brightness),255);
+                labelNode.setColor(fadedColour);
+            }
+        }
+    }
+
+
+    @AllArgsConstructor
+    private abstract static class LineItem{
+        String label;
+
+        public abstract Node render();
+
+        public void update(double timeslice){
+
+        }
     }
 }
