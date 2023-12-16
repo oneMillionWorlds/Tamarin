613db60e3befc42fd464e99b247b643147ef6867
Richard Tingle <98174673+oneMillionWorlds@users.noreply.github.com>
2023-12-16 10:38:33 +0000

    Revert "Ensure that the bloom post processor works"
    This reverts commit f676411ff7d87a58e3cdd7460f928323a3b27da3.

--- src/main/java/com/onemillionworlds/tamarin/openxr/XrAppState.java
+++ src/main/java/com/onemillionworlds/tamarin/openxr/XrAppState.java
@@ -40,10 +40,6 @@ public class XrAppState extends BaseAppState{
     @Getter
     ViewPort rightViewPort;
 
-    DelegatingFrameBuffer leftFrameBuffer = new DelegatingFrameBuffer();
-
-    DelegatingFrameBuffer rightFrameBuffer = new DelegatingFrameBuffer();
-
     /**
      * The observer's position in the virtual world maps to the VR origin in the real world.
      */
@@ -112,14 +108,9 @@ public class XrAppState extends BaseAppState{
 
         leftViewPort = app.getRenderManager().createMainView("Left Eye", leftCamera);
         leftViewPort.setClearFlags(true, true, true);
-        leftFrameBuffer.setDelegatedFrameBuffer(leftViewPort.getOutputFrameBuffer());
-        leftViewPort.setOutputFrameBuffer(leftFrameBuffer);
 
         rightViewPort = app.getRenderManager().createMainView("Right Eye", rightCamera);
         rightViewPort.setClearFlags(true, true, true);
-        rightFrameBuffer.setDelegatedFrameBuffer(rightViewPort.getOutputFrameBuffer());
-        rightViewPort.setOutputFrameBuffer(rightFrameBuffer);
-
         leftViewPort.attachScene(((SimpleApplication) app).getRootNode());
         rightViewPort.attachScene(((SimpleApplication) app).getRootNode());
 
@@ -211,9 +202,8 @@ public class XrAppState extends BaseAppState{
                 updateEyePositions(inProgressXrRender);
             }
             //must set every frame due to OpenXR buffering to multiple images
-
-            leftFrameBuffer.setDelegatedFrameBuffer(inProgressXrRender.getLeftBufferToRenderTo());
-            rightFrameBuffer.setDelegatedFrameBuffer(inProgressXrRender.getRightBufferToRenderTo());
+            leftViewPort.setOutputFrameBuffer(inProgressXrRender.getLeftBufferToRenderTo());
+            rightViewPort.setOutputFrameBuffer(inProgressXrRender.getRightBufferToRenderTo());
 
             if (refreshProjectionMatrix || !inProgressXrRender.leftEye.fieldOfView().equals(leftFovLastRendered) || !inProgressXrRender.rightEye.fieldOfView().equals(rightFovLastRendered)){
                 leftCamera.setProjectionMatrix(inProgressXrRender.leftEye.calculateProjectionMatrix(nearClip, farClip));
