3f43a6820e6408b72ceabc6e2dc379101c90e069
Richard Tingle <98174673+oneMillionWorlds@users.noreply.github.com>
2024-09-06 10:55:39 +0100

    Reinstate Snap Callback (deprecated_ for backwards compatibility)

--- src/main/java/com/onemillionworlds/tamarin/vrhands/grabbing/snaptopoints/SnapToConfiguration.java
+++ src/main/java/com/onemillionworlds/tamarin/vrhands/grabbing/snaptopoints/SnapToConfiguration.java
@@ -7,6 +7,7 @@ import com.onemillionworlds.tamarin.vrhands.grabbing.restrictions.RestrictionUti
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 public class SnapToConfiguration{
 
@@ -35,15 +36,42 @@ public class SnapToConfiguration{
     }
 
     /**
-     * Given a position, this will snap it to the nearest point in the list of points (if any are within range).
-     * If multiple points are within range, the closest one will be chosen.
-     * <p>
-     * In all cases the argument and return values are in local space.
-     * </p>
-     * <p>
-     *     Will call onSnap and onUnsnap callbacks as appropriate (so this is a stateful operation).
-     * </p>
+     * Deprecated, use {@link SnapToConfiguration#SnapToConfiguration(Collection, SnapChangeCallback)} instead
+     *
+     * @param snapTargets The points to snap to (and the ranges at which they snap)
+     * @param onSnap Called when a point is snapped to
+     *               (both the global and local snapped to positions are provided)
+     * @param onUnsnap Called when a point is unsnapped from
      */
+    @Deprecated(since="2.6", forRemoval = true)
+    public SnapToConfiguration( Collection<SnapTarget> snapTargets, OnSnapCallback onSnap, OnUnSnapCallback onUnsnap){
+        this(
+            snapTargets,
+            new SnapChangeCallback(){
+                @Override
+                public void onSnapFromUnsnapped(BoundHand handGrabbing, Vector3f snappedToLocal, Vector3f snappedToGlobal){
+                    onSnap.onSnap(handGrabbing, snappedToLocal, snappedToGlobal);
+                }
+
+                @Override
+                public void onUnsnapFromSnapped(BoundHand handGrabbing){
+                    onUnsnap.onUnSnap(handGrabbing);
+                }
+            }
+        );
+    }
+
+
+        /**
+         * Given a position, this will snap it to the nearest point in the list of points (if any are within range).
+         * If multiple points are within range, the closest one will be chosen.
+         * <p>
+         * In all cases the argument and return values are in local space.
+         * </p>
+         * <p>
+         *     Will call onSnap and onUnsnap callbacks as appropriate (so this is a stateful operation).
+         * </p>
+         */
     public Optional<Vector3f> snap(Vector3f position, RestrictionUtilities restrictionUtilities, BoundHand boundHand){
         if(snapTargets.isEmpty()){
             return Optional.empty();
@@ -87,6 +115,52 @@ public class SnapToConfiguration{
 
         isSnappedTo = newSnappedTarget;
         return snapToPoint;
+    }
+
+    /**
+     * Deprecated. Use {@link SnapChangeCallback} instead.
+     */
+    @Deprecated(since="2.6", forRemoval = true)
+    public interface OnSnapCallback{
+        OnSnapCallback NO_OP = (boundHand, snappedToLocal, snappedToGlobal) -> {};
+
+        void onSnap(BoundHand handGrabbing, Vector3f snappedToLocal, Vector3f snappedToGlobal);
+    }
+
+    /**
+     * Deprecated. Use {@link SnapChangeCallback} instead.
+     */
+    @Deprecated(since="2.6", forRemoval = true)
+    public interface OnUnSnapCallback{
+        OnUnSnapCallback NO_OP = (boundHand) -> {};
+
+        void onUnSnap(BoundHand handGrabbing);
+    }
+
+    /**
+     * Deprecated. Use {@link SnapChangeCallback} instead.
+     */
+    @Deprecated(since="2.6", forRemoval = true)
+    public static SnapSimpleCallback simpleCallback(Consumer<BoundHand> callback){
+        return callback::accept;
+    }
+
+    /**
+     * Deprecated. Use {@link SnapChangeCallback} instead.
+     */
+    @Deprecated(since="2.6", forRemoval = true)
+    public interface SnapSimpleCallback extends OnSnapCallback, OnUnSnapCallback{
+        void callback(BoundHand hand);
+
+        @Override
+        default void onSnap(BoundHand handGrabbing, Vector3f snappedToLocal, Vector3f snappedToGlobal){
+            callback(handGrabbing);
+        }
+
+        @Override
+        default void onUnSnap(BoundHand handGrabbing){
+            callback(handGrabbing);
+        }
     }
 
 }
